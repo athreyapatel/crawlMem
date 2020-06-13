ea82aacc5255fd477e5a9794b2e7e6076d56502a
Athreyapatel <athreyapatel@gmail.com>
2020-06-14 01:34:39 +0530

    workerthread methods

--- hello.js
+++ hello.js
@@ -1,9 +1,16 @@
-const { Worker, isMainThread } = require('worker_threads');
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 //isMainThread helps us know when we are either running inside the main thread or a worker thread
 //new Worker(__filename) registers a new worker with the __filename variable which, in this case, is hello.js
 
 if (isMainThread) {
-  new Worker(__filename);
+  const worker = new Worker(__filename);
+  worker.once('message', (message) => {
+    console.log(message);
+  });
+  worker.postMessage('Main thread');
 } else {
-  console.log('Worker says: Its working');
+  parentPort.once('message', (message) => {
+    console.log(message);
+    parentPort.postMessage('worker thread');
+  });
 }
