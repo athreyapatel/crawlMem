a2ad53ac38e956bbee794144bb9a8489075b1969
Athreyapatel <athreyapatel@gmail.com>
2020-06-14 14:35:56 +0530

    refactor

--- main.js
+++ main.js
@@ -2,19 +2,37 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const url = 'https://www.iban.com/exchange-rates';
 
-fetchData(url).then((res) => {
-  const html = res.data;
-  const $ = cheerio.load(html);
-  const statsTable = $(
+
+let workDir = __dirname + './dbWorker.js';
+
+const mainFunc = ()=>{
+   const url = 'https://www.iban.com/exchange-rates';
+   let res = await fetchData(url);
+   if(!res.data){
+       console.log("Invalid data");
+       return;
+   }
+   const html = res.data;
+   let dataObject = new Object();
+   const $ = cheerio.load(html);
+   const statsTable = $(
     '.table.table-bordered.table-hover.downloads > tbody > tr'
-  );
-  statsTable.each(function () {
+   );
+   statsTable.each(function () {
     let title = $(this).find('td').text();
+    let newStr = title.split("\t"); //convert text to array
+    newStr.shift(); //strip off empty arrat at index 0
+    formatStr(newStr, dataObject); //format array string and store in object
     console.log(title);
-  });
+   });
+   return dataObject;
+}
+
+mainFunc().then((res)=>{
+    
 });
+
 async function fetchData(url) {
   console.log('Crawl Data');
   //making http call to url
@@ -25,3 +43,5 @@ async function fetchData(url) {
   }
   return response;
 }
+
+
